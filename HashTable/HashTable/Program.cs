55d142a0f207c57a7499ab883c2d4f4c93ed3ba0
roshni1015 <roshniadatrao1110@gmail.com>
2022-05-14 16:42:34 +0530

    [Roshni]-Finding Frequency in Large Paragraph

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -1,12 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using HashTable;
 
-HashTable<string, int> hashTable = new(6);
-hashTable.Insert("To", 1);
-hashTable.Insert("be", 2);
-hashTable.Insert("or", 3);
-hashTable.Insert("not", 4);
-hashTable.Insert("to", 5);
-hashTable.Insert("be", 6);
+while (true)
+{
+    Console.WriteLine("Please choose the option :\n1)Finding Frequency \n2)Finding Frequency of Words in a Large Paragraph");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            HashTable<string, int> hashTable = new(6);
+            hashTable.Insert("To", 1);
+            hashTable.Insert("be", 2);
+            hashTable.Insert("or", 3);
+            hashTable.Insert("not", 4);
+            hashTable.Insert("to", 5);
+            hashTable.Insert("be", 6);
+            Console.WriteLine("Value of key 'not' is: " + hashTable.GetValue("not"));
+            break;
+        case 2:
+            string GivenPara = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] WordsInArray = GivenPara.Split(" ");
+            int tableSize = WordsInArray.Length;
+            HashTable<int, string> hashTable1 = new(tableSize);
+            int Index = 0;
+            foreach (string words in WordsInArray)
+            {
+                hashTable1.Insert(Index, words);
+                Index++;
+            }
+            for (Index = 0; Index < tableSize; Index++)
+            {
+                string word = hashTable1.GetValue(Index);
+                Console.WriteLine(word + "-->" + Index);
 
-Console.WriteLine("Value of key 'be' is: " + hashTable.GetValue("be"));
+            }
+            break;
+        default:
+            Console.WriteLine("Please choose the correct option!");
+            break;
+
+    }
+}
