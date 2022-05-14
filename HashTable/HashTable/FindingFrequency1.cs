2d72c131c9d952276b64af3311c52cb7e31b7722
roshni1015 <roshniadatrao1110@gmail.com>
2022-05-14 16:32:41 +0530

    [Roshni]-Finding Frequency

---
+++ HashTable/HashTable/FindingFrequency1.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class FindingFrequency<T1, T2>
+{
+    private int v;
+
+    public FindingFrequency(int v)
+    {
+        this.v = v;
+    }
+}
--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to HashTable Operation!");
+using HashTable;
+
+HashTable<string, int> hashTable = new(6);
+hashTable.Insert("To", 1);
+hashTable.Insert("be", 2);
+hashTable.Insert("or", 3);
+hashTable.Insert("not", 4);
+hashTable.Insert("to", 5);
+hashTable.Insert("be", 6);
+
+Console.WriteLine("Value of key 'be' is: " + hashTable.GetValue("be"));
