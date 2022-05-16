d7519eb2a35bc5cae4913986b39324d21f1cc1ec
roshni1015 <99880059+roshni1015@users.noreply.github.com>
2022-05-16 13:46:50 +0530

    Update Program.cs

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -1,43 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using HashTable;
 
+Console.WriteLine("Welcome to HashTable Operation!");
 while (true)
 {
     Console.WriteLine("Please choose the option :\n1)Finding Frequency \n2)Finding Frequency of Words in a Large Paragraph");
-    int option = Convert.ToInt32(Console.ReadLine());
+    int option = Convert.ToInt16(Console.ReadLine());
     switch (option)
     {
         case 1:
-            HashTable<string, int> hashTable = new(6);
-            hashTable.Insert("To", 1);
-            hashTable.Insert("be", 2);
-            hashTable.Insert("or", 3);
-            hashTable.Insert("not", 4);
-            hashTable.Insert("to", 5);
-            hashTable.Insert("be", 6);
-            Console.WriteLine("Value of key 'not' is: " + hashTable.GetValue("not"));
+            HashTable<string, int> hash = new HashTable<string, int>(5);
+            string Words = "To be or not to be";
+            string[] array = Words.Split(' ');
+            LinkedList<string> checkForDuplicationS = new LinkedList<string>();
+            foreach (string element in array)
+            {
+                int count = 0;
+                foreach (string match in array)
+                {
+                    if (element == match)
+                    {
+                        count++;
+                        if (checkForDuplicationS.Contains(element))
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (!checkForDuplicationS.Contains(element))
+                {
+                    checkForDuplicationS.AddLast(element);
+                    hash.Add(element, count);
+                }
+            }
+            hash.Display();
             break;
         case 2:
-            string GivenPara = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-            string[] WordsInArray = GivenPara.Split(" ");
-            int tableSize = WordsInArray.Length;
-            HashTable<int, string> hashTable1 = new(tableSize);
-            int Index = 0;
-            foreach (string words in WordsInArray)
+            HashTable<string, int> hash2 = new HashTable<string, int>(5);
+            string Words2 = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] array2 = Words2.Split(' ');
+            LinkedList<string> checkForDuplication2 = new LinkedList<string>();
+            foreach (string element in array2)
             {
-                hashTable1.Insert(Index, words);
-                Index++;
+                int count = 0;
+                foreach (string match in array2)
+                {
+                    if (element == match)
+                    {
+                        count++;
+                        if (checkForDuplication2.Contains(element))
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (!checkForDuplication2.Contains(element))
+                {
+                    checkForDuplication2.AddLast(element);
+                    hash2.Add(element, count);
+                }
             }
-            for (Index = 0; Index < tableSize; Index++)
-            {
-                string word = hashTable1.GetValue(Index);
-                Console.WriteLine(word + "-->" + Index);
-
-            }
+            hash2.Display();
             break;
         default:
             Console.WriteLine("Please choose the correct option!");
             break;
-
     }
 }
